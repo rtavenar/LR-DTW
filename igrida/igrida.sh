94bceb8884b076225ea248bcabefdb356db85c71
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2016-04-14 15:04:46 +0200

    Igrida

---
+++ igrida/igrida.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#OAR -l {mem_core < 2048}/nodes=1/core=3,walltime=168:00:00
+#OAR -l {mem_core > 2048}/nodes=1/core=1,walltime=168:00:00
+#OAR --array-param-file /udd/rtavenar/src/LR-DTW/igrida/params.data
+#OAR -O /temp_dd/igrida-fs1/rtavenar/LR-DTW/output/%jobid%.output
+#OAR -E /temp_dd/igrida-fs1/rtavenar/LR-DTW/output/%jobid%.error
+# set -xv
+
+/udd/rtavenar/src/LR-DTW/c/test_1nn $*
+
+
---
+++ igrida/params.py
@@ -0,0 +1,16 @@
+import os
+
+__author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
+
+path = "../xp/ucr_all/"
+
+fp_out = open("params.data", "w")
+for ds_name in os.listdir(path):
+    if os.path.isdir(os.path.join(path, ds_name)):
+        fname_train = os.path.join(path, ds_name, "%s_TRAIN" % ds_name)
+        fname_test = os.path.join(path, ds_name, "%s_TEST" % ds_name)
+        if os.path.exists(fname_train) and os.path.exists(fname_test):
+            for gamma in [0., 1., 5., 10., 50., 100.]:
+                for entropy_reg in [0, 1]:
+                    fp_out.write("%s %s %f %d\n" % (fname_train, fname_test, gamma, entropy_reg))
+fp_out.close()
